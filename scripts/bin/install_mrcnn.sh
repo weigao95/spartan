a8c8f8611f135bcff39a4b6ae72cf6c7a6210948
wei <gaowei19951004@gmail.com>
2019-05-16 23:21:05 -0400

    more update

--- scripts/bin/install_mrcnn.sh
+++ scripts/bin/install_mrcnn.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 install_maskrcnn()
-{   cd ~/maskrcnn_src_install/maskrcnn-benchmark
+{   
+    cd ${HOME}
+    mkdir maskrcnn_src_install
+    cd maskrcnn_src_install
+    git clone https://github.com/weigao95/maskrcnn-benchmark.git
+    cd ~/maskrcnn_src_install/maskrcnn-benchmark
     sudo python setup.py build
     sudo python setup.py install
 }
